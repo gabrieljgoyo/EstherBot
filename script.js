7e96b325e2d74012d783c848fc78a208f6411195
gabrieljgoyo <gabriel.goyo@gmail.com>
2016-05-21 10:38:37 -0430

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('¡GIR, listo para servir! ¿Así que quieres aprender más sobre Gabriel? Sólo escribe "HOLA" para comenzar.')
                 .then(() => 'speak');
         }
     },
